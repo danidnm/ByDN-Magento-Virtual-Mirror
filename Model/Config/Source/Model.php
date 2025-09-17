e68893b437c7f5cc63f1595c006b7ba6916429f6
Daniel Navarro <danidnm@gmail.com>
2025-09-17 22:31:27 +0200

    Add License headers

--- Model/Config/Source/Model.php
+++ Model/Config/Source/Model.php
@@ -1,5 +1,14 @@
 <?php
-
+/**
+ * @package     Bydn_VirtualMirror
+ * @author      Daniel Navarro <https://github.com/danidnm>
+ * @license     GPL-3.0-or-later
+ * @copyright   Copyright (c) 2025 Daniel Navarro
+ *
+ * This file is part of a free software package licensed under the
+ * GNU General Public License v3.0.
+ * You may redistribute and/or modify it under the same license.
+ */
 namespace Bydn\VirtualMirror\Model\Config\Source;
 
 class Model implements \Magento\Framework\Option\ArrayInterface
